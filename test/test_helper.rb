67e4a850723e9bb22aac3bfec0b35a6c2df0df59
Geoff Hubbard <ghub@instihost.com>
2017-10-11 08:52:44 +0100

    Create the tmp directory if it does not exist.
    On a fresh git checkout the 'tmp' directory used for storing test output
    images does not exist. This adds a line to the test_helper to create the
    directory if necessary.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,7 @@ SimpleCov.start
 
 require 'minitest/autorun'
 require_relative '../lib/rhex'
+
+# Ensure the image output directory exists
+IMAGE_OUTPUT_DIRECTORY = 'tmp'
+Dir.mkdir(IMAGE_OUTPUT_DIRECTORY) unless Dir.exist?(IMAGE_OUTPUT_DIRECTORY)
